c46614fd3fbdd326cadc7c8d1fb8e50a962099e8
hyong0117 <103429879+dhxogud@users.noreply.github.com>
2025-01-02 16:00:44 +0900

    Project first Setting

---
+++ Assets/Scripts/Unit/Controllable/UnitController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+
+    [SerializeField]
+    int _hp;
+    int _attack;
+    int _speed;
+
+    
+}
