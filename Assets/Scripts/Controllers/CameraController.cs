85c8d73cccfa2a5dfd17497b45bc6cfbaaa2835f
hyong0117 <103429879+dhxogud@users.noreply.github.com>
2025-01-02 19:24:44 +0900

    CameraController coding

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -1,8 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-   
+    [SerializeField]
+    GameObject _target;
+    void Update() 
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, 100.0f))
+            {
+                _target = hit.collider.gameObject;
+            }
+        }
+        MoveToTarget();
+    }
+
+    void MoveToTarget()
+    {
+        if (_target == null) 
+            return;
+
+        Vector3 dist = _target.transform.position - transform.position;
+        Debug.Log(dist.magnitude);
+        if (dist.magnitude > 10.0f)
+        {
+            transform.position = Vector3.Slerp(transform.position, dist, 1.0f);
+            transform.LookAt(_target.transform);
+        }
+    }
 }
---
+++ Assets/Scripts/Utils/Define.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+    public enum Mouse
+    {
+        Click,
+        Press,
+        Drag
+    }
+
+    public enum Objects
+    {
+        Unit,
+        Enemy,
+        Environment,
+        Field
+    }
+}
