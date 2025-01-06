95306fd2d5ef97df8f54e12e400591fcb9be5e82
hyong0117 <103429879+dhxogud@users.noreply.github.com>
2025-01-07 01:31:05 +0900

    Feature/Managers와InputManager설계

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -8,7 +8,7 @@ using UnityEngine.UIElements;
 
 public class CameraController : MonoBehaviour
 {
-    // *** 타겟과의 최소 거리는 10~20으로 *** // 
+    // *** 타겟과의 최소 거리는 5~20으로 *** // 
     // 카메라는 WASD, QE(카메라 y축 -+ 회전), 휠(Zoom-in, Zoom-out), 클릭 와 같은 Input event로 움직인다.
     // 또한 내가 선택한 케릭터가 적 케릭터를 '공격'할 때 타겟팅이 되었다는 것을 보여주기 위해 해당 타겟 유닛을 포커스 하러 움직인다.
 
@@ -22,9 +22,19 @@ public class CameraController : MonoBehaviour
     public Camera _camera;
     [SerializeField]
     Define.CameraMode _mode = Define.CameraMode.QuarterView;
+
     [SerializeField]
     GameObject _target = null;
 
+    void Start() 
+    {
+        Managers.Input.MouseAction -= OnMouseWheel;
+        Managers.Input.MouseAction += OnMouseWheel;
+        Managers.Input.MouseAction -= OnMouseClicked;
+        Managers.Input.MouseAction += OnMouseClicked;
+        Managers.Input.KeyAction -= OnKeyboard;
+        Managers.Input.KeyAction += OnKeyboard;
+    }
     void LateUpdate() 
     {
         if (Input.GetMouseButtonDown(0))
@@ -35,16 +45,13 @@ public class CameraController : MonoBehaviour
             if (Physics.Raycast(ray.origin, ray.direction, out hit, 100.0f))
             {
                 _target = hit.collider.gameObject;
-                Debug.Log(Vector3.Distance(transform.position, _target.transform.position));
             }
         }
-        Zoom();
-        TurnAngle();
-        MoveToTarget();
     }
 
-    void MoveToTarget() // 이거 1초 동안 움직이게 만들고 싶다. 이름도 바꿀거임 [타겟 선택] 이런 느낌으로
+    void OnMouseClicked(Define.MouseEvent evt) // 이거 1초 동안 움직이게 만들고 싶다. 이름도 바꿀거임 [타겟 선택] 이런 느낌으로
     {
+        Vector3 mouse = Input.mousePosition;
         if (_target == null) 
             return;
 
@@ -59,11 +66,10 @@ public class CameraController : MonoBehaviour
     // 대충 축의 거리는 8.0f 가 적당해 보이므로 이 값을 defalut로 두겠음 (기본 box object의 sclae 1일때를 기준)
     // 근데 생각해보니 내가 이제 맵의 노드들을 분리할때 그 노드의 위치벡터 기준으로 거리를 제야하니깐 지금은 정확한 수치를 마련 못하는 것도 당연하네?
     // 나중에 targetAxis 저 아래 10.0f 란 수치가 아니라. Vector3.Distance(transform.position, MapNode node...) 막 이런식으로 구해야겟듬
-    void TurnAngle()
+    void OnKeyboard()
     {
         if (Input.GetKey(KeyCode.Q))
         {
-
             transform.position += transform.right * 10.0f * Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.E))
@@ -77,12 +83,12 @@ public class CameraController : MonoBehaviour
 
     }
     
-    // 마우스 위쪽으로 휠 돌리면 Vector2(null, +value) 되고
-    // 마우스 아랫쪽으로 휠 돌리면 Vector2(null, -value) 되는걸 확인 마우스 휠 처리도 나중에 Action에 넣어서 ㄱㄱ
-    // Field of View 값 변경해서 zoom 기능 구현할 수 있음 값은 (30~60)... 인데 이거 문제 되는거같다고 함? 카메라가 직접 앞뒤로 이동해야 맞는듯
-    void Zoom()
+    // Zoom 했을때, 카메라가 바라보고 있는 방향으로 직선으로 이동하는게 아니라 2차 곡선을 그리듯이 접근하는 방식 ㄱㄱ
+    // 이러면 카메라의 FOV 값을 건드리는게 아니라, 위치와 각도를 정해진 범위에 맞게 수정해야됨
+    // 타겟과의 거리는 5~20 으로 제한
+    void OnMouseWheel(Define.MouseEvent evt)
     {
-        
+        Vector3 dist = transform.position;
     }
 
     void Init()
