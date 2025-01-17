661cb416317c4687bca06de8055e359adad442a2
hyong0117 <103429879+dhxogud@users.noreply.github.com>
2025-01-18 04:21:30 +0900

    실수

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -9,18 +9,17 @@ public class CameraController : MonoBehaviour
     // 아래 멤버 변수들은 지울수도 잇음
     GameObject _target = null;
     Vector3 _dir = new Vector3(0.0f, 7.0f, -12.0f);
-    float _moveSpeed = 30.0f;
-    float _rotSpeed = 10.0f;
+    float _moveSpeed = 10.0f;
+    float _rotAngle = 10.0f;
 
-    enum GameState
-    {
-        Lobby,
-        Game,
-
-    }
+    private enum Keys  {   w, a, s, d, q, e   }
+    bool[] onKeys = new bool[ Enum.GetValues(typeof(Keys)).Length ];
 
     void Start() 
     {
+        for (int i = 0; i < Enum.GetValues(typeof(Keys)).Length; i++)
+            onKeys.SetValue(false, i);
+
         Managers.Input.MouseAction -= OnMouseClick;
         Managers.Input.MouseAction += OnMouseClick;
         Managers.Input.KeyAction -= OnKeyBoard;
@@ -45,8 +44,37 @@ public class CameraController : MonoBehaviour
 
     void OnKeyBoard()
     {
-        _moveSpeed = Math.Clamp(_moveSpeed * Time.deltaTime, 0.5f, 3.0f);
-        _rotSpeed = Math.Clamp(_rotSpeed * Time.deltaTime, 0.5f, 1.0f);
+        if (Input.GetKey(KeyCode.W))
+            onKeys[(int) Keys.w] = true;
+        if (Input.GetKey(KeyCode.S))
+            onKeys[(int) Keys.s] = true;
+        if (Input.GetKey(KeyCode.D))
+            onKeys[(int) Keys.d] = true;
+        if (Input.GetKey(KeyCode.A))
+            onKeys[(int) Keys.a] = true;
+        if (Input.GetKey(KeyCode.Q))
+            onKeys[(int) Keys.q] = true;
+            
+        if (Input.GetKey(KeyCode.E))
+            onKeys[(int) Keys.e] = true;
+
+        
+
+        _rotAngle = Math.Clamp(_rotAngle * Time.deltaTime, 0.5f, _rotAngle);
+
+        if (Input.GetKey(KeyCode.Q))
+            transform.rotation = Quaternion.Euler(transform.eulerAngles + (Vector3.up * _rotAngle));
+        else if (Input.GetKey(KeyCode.E))
+            transform.rotation = Quaternion.Euler(transform.eulerAngles + (Vector3.down * _rotAngle));
+    }
+
+    void LateUpdate() 
+    {
+        Move();
+    }
+    void Move()
+    {
+        _moveSpeed = Math.Clamp(_moveSpeed * Time.deltaTime, 0.5f, _moveSpeed);
 
         Vector3 moveDir = Vector3.zero;
         if (Input.GetKey(KeyCode.W))
@@ -58,16 +86,7 @@ public class CameraController : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
             moveDir += transform.right * -1;
         
-        if (moveDir.magnitude != 0.0f)
-        {
-            moveDir.y = 0.0f;
-            transform.position += moveDir.normalized * _moveSpeed;
-        }
-        
-        if (Input.GetKey(KeyCode.Q))
-            transform.rotation = Quaternion.Euler(transform.eulerAngles + (Vector3.up * _rotSpeed));
-        else if (Input.GetKey(KeyCode.E))
-            transform.rotation = Quaternion.Euler(transform.eulerAngles + (Vector3.down * _rotSpeed));
-
+        moveDir.y = 0.0f;
+        transform.position += moveDir.normalized * _moveSpeed;
     }
 }
