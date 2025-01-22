55fd9e3336086ac590c93c787c57d656254a3ce6
hyong0117 <103429879+dhxogud@users.noreply.github.com>
2025-01-23 00:23:13 +0900

    CameraController작성

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -8,22 +8,18 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField]
     Define.CameraMode _mode = Define.CameraMode.QuarterView;
-    [SerializeField]
     Vector3 _look = Vector3.zero;
-
+    [SerializeField]
     GameObject _target;
 
-    const float _minHeight = 3.0f, _maxHeight = 10.0f;
-    float _speed = 10.0f;
-    float _angle = 1.0f;
+    const float _minYPos = 5.0f, _maxYPos = 30.0f;
 
     void Start() 
     {
-        Managers.Input.MouseAction -= TargetOn;
-        Managers.Input.MouseAction += TargetOn;
+        Managers.Input.MouseAction -= MouseClickAction;
+        Managers.Input.MouseAction += MouseClickAction;
     }
-
-    void TargetOn(Define.MouseEvent evt)
+    void MouseClickAction(Define.MouseEvent evt)
     {
         if (evt != Define.MouseEvent.Click)
             return;
@@ -37,33 +33,36 @@ public class CameraController : MonoBehaviour
             }
         }
     }
-
     void LateUpdate() 
     {
         if (_mode == Define.CameraMode.QuarterView)
         {
             if (_target != null)
             {
-                Vector3 prev = transform.position;
-                Vector3 dest = transform.position + (_target.transform.position - _look);
-                transform.position = Vector3.Slerp(transform.position, dest, 0.1f);
-                _look += transform.position - prev;
-
-                if (Vector3.Distance(_target.transform.position, _look) < 0.001f)
-                {
-                    _target = null;
-                }
-            }
-
-            else
-            {
                 ParallelMove();
                 OrbitalMove();
                 Zoom();
             }
+            else
+            {
+                MoveToTarget();
+            }
         }
     }
+    void MoveToTarget()
+    {
+        Vector3 prev = transform.position;
+        Vector3 dest = _target.transform.position - _look;
+        transform.position = Vector3.Slerp(transform.position, dest, 0.1f);
+        _look += transform.position - prev;
 
+        Debug.DrawLine(transform.position, _look, Color.red, 10.0f);
+
+        if (Vector3.Distance(transform.position, dest) < 0.001f)
+        {
+            _target = null;
+        }
+    }
     void ParallelMove()
     {
         Vector3 moveDir = Vector3.zero;
@@ -75,34 +74,38 @@ public class CameraController : MonoBehaviour
             moveDir += transform.right;
         if (Input.GetKey(KeyCode.A))
             moveDir += transform.right * -1;
-        moveDir.y = 0.0f;
-        moveDir = moveDir.normalized * _speed * Time.deltaTime;
 
+        moveDir.y = 0.0f;
+        moveDir = moveDir.normalized * 30.0f * Time.deltaTime;
         transform.position += moveDir;
-        _look += moveDir;
+        _look += moveDir; // 
     }
     
     void OrbitalMove()
     {
         Vector3 delta = transform.position - _look;
         if (Input.GetKey(KeyCode.Q))
-            delta = Quaternion.AngleAxis(_angle, Vector3.up) * delta;
+            delta = Quaternion.AngleAxis(1.0f, Vector3.up) * delta;
         else if (Input.GetKey(KeyCode.E))
-            delta = Quaternion.AngleAxis(_angle, Vector3.down) * delta;
+            delta = Quaternion.AngleAxis(1.0f, Vector3.down) * delta;
 
         transform.position = _look + delta;
         transform.LookAt(_look);
     }
-
     void Zoom()
     {
         float delta = Input.mouseScrollDelta.y;
-        if (delta == 0) return;
-
-        if (delta > 0 && transform.position.y < _maxHeight)
-            transform.position += Vector3.up;
-        else if (delta < 0 && transform.position.y > _minHeight)
-            transform.position += Vector3.down;
-        transform.LookAt(_look);
+        if (delta > 0 && transform.position.y < _maxYPos)
+        {
+            Vector3 dir = new Vector3(0.0f, delta, 0.0f) * 30.0f * Time.deltaTime;
+            transform.position += dir;
+            _look += dir;
+        }
+        else if (delta < 0 && transform.position.y > _minYPos)
+        {
+            Vector3 dir = new Vector3(0.0f, delta, 0.0f) * 30.0f * Time.deltaTime;
+            transform.position += dir;
+            _look += dir;
+        }
     }
 }
